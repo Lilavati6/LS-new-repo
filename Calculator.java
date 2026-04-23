8996e050dbc42fd7c9a4091dc0a5254fa6fee0af
Lilavati6 <shirodkarlilavati@yahoo.in>
2026-04-23 11:49:58 +0530

    Create Calculator.java
    calculator Java

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+// File: Calculator.java
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
